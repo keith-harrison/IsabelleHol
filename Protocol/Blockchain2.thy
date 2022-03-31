b9f26484f69fea6da83df912e8b7721d5503b36c
Keith Harrison <56916474+keith-harrison@users.noreply.github.com>
2022-03-31 14:26:39 +0100

    14:26 31/03
    Changed Model think extend elmma works

--- Protocol/Blockchain2.thy
+++ Protocol/Blockchain2.thy
@@ -74,9 +74,11 @@ fun allBlocks :: "T \<Rightarrow> BlockPool" where
 "allBlocks (Node m l r) = allBlocks l @ allBlocks r @ [m]"|
 "allBlocks Leaf = []"
 
+(*
 fun allBlocksGen :: "T \<Rightarrow> BlockPool" where
 "allBlocksGen (Node m l r) = (if m = GenBlock then allBlocks l@allBlocks r @[m]else [])"|
 "allBlocksGen Leaf = []"
+*)
 
 fun allBlocksAppend :: "Block \<Rightarrow>BlockPool list\<Rightarrow> BlockPool list" where
 "allBlocksAppend Bl BlP = (map (\<lambda> bl. bl @ [Bl]) BlP)"
@@ -84,13 +86,13 @@ fun allBlocksAppend :: "Block \<Rightarrow>BlockPool list\<Rightarrow> BlockPool
 fun allBlocks' :: "T \<Rightarrow> BlockPool list" where
 "allBlocks' (Node m l r) = allBlocksAppend m (allBlocks' l) @allBlocksAppend m (allBlocks' r)"|
 "allBlocks' Leaf = [[]]"
-
+(*
 fun allBlocksGen' :: "T \<Rightarrow> BlockPool list" where
 "allBlocksGen' (Node m l r) = (if m = GenBlock then allBlocksAppend m (allBlocks' l) @allBlocksAppend m (allBlocks' r) else [])"
-
+*)
 definition "tree0 = Node GenBlock Leaf Leaf"
 
-lemma initialTree : "allBlocksGen tree0 = [GenBlock]" 
+lemma initialTree : "allBlocks tree0 = [GenBlock]" 
   by (simp add: GenBlock_def tree0_def)
 
 fun extendTree :: "T \<Rightarrow> Block \<Rightarrow> T" where
@@ -102,22 +104,25 @@ fun extendTree :: "T \<Rightarrow> Block \<Rightarrow> T" where
 value "valid_blocks Block1 GenBlock"
 value "valid_chain [Block1,GenBlock]"
 value "Block1 # [GenBlock]"
+(*
 fun extendTreeGen :: "T \<Rightarrow> Block \<Rightarrow> T" where
 "extendTreeGen (Node Bl1 Leaf Leaf) Bl2 = (if valid_chain (Bl2#[Bl1]) then (Node Bl1 (Node Bl2 Leaf Leaf) Leaf) else (Node Bl1 Leaf Leaf)) "|
 "extendTreeGen (Node Bl1 t1 Leaf) Bl2 =  (if valid_chain (Bl2#[Bl1])  then (Node Bl1 t1 (Node Bl2 Leaf Leaf)) else (if Bl1 = GenBlock then (Node Bl1 (extendTree t1 Bl2) Leaf) else (Node Bl1 t1 Leaf)))"|
 "extendTreeGen (Node Bl1 Leaf t1) Bl2 =  (if valid_chain (Bl2#[Bl1]) then (Node Bl1 (Node Bl2 Leaf Leaf) t1) else (if Bl1 = GenBlock then (Node Bl1  Leaf (extendTree t1 Bl2)) else (Node Bl1 Leaf t1)))"|
 "extendTreeGen (Node Bl1 t1 t2) Bl2 = (if Bl1 = GenBlock then (Node Bl1 (extendTree t1 Bl2) (extendTree t2 Bl2)) else (Node Bl1 t1 t2)) "|
 "extendTreeGen Leaf Bl2 = Leaf "
-lemma "extendTreeGen Leaf B = Leaf"
+*)
+
+lemma "extendTree Leaf B = Leaf"
   by simp 
 
 
   
 
-value "(extendTreeGen tree0 Block1) = (Node GenBlock (Node Block1 Leaf Leaf)  Leaf)"
-value "extendTreeGen (T.Node \<lparr>sl = 0, txs = 0, pred = H 0 0, bid = 0\<rparr> T.Leaf (T.Node \<lparr>sl = 1, txs = 0, pred = H 0 0, bid = 0\<rparr> T.Leaf T.Leaf))  \<lparr>sl =1 , txs = 0, pred = H 0 0, bid = 0\<rparr> "
+value "(extendTree tree0 Block1) = (Node GenBlock (Node Block1 Leaf Leaf)  Leaf)"
+value "extendTree (T.Node \<lparr>sl = 0, txs = 0, pred = H 0 0, bid = 0\<rparr> T.Leaf (T.Node \<lparr>sl = 1, txs = 0, pred = H 0 0, bid = 0\<rparr> T.Leaf T.Leaf))  \<lparr>sl =1 , txs = 0, pred = H 0 0, bid = 0\<rparr> "
 value "valid_chain  (\<lparr>sl = 0, txs = 0, pred = H 0 0, bid = 0\<rparr>#[\<lparr>sl = 0, txs = 0, pred = H 0 0, bid = 1\<rparr>])"
-lemma ExtendInitial : "(extendTreeGen tree0 Block1) = (Node GenBlock (Node Block1 Leaf Leaf) Leaf)"
+lemma ExtendInitial : "(extendTree tree0 Block1) = (Node GenBlock (Node Block1 Leaf Leaf) Leaf)"
   by (simp add: GenBlock_def tree0_def Block1_def)
 
 definition valid_t where
@@ -155,72 +160,49 @@ next
   qed
 qed
 
-lemma GenExtend : "t \<noteq> Leaf\<and>(extendTreeGen t b \<noteq> t)\<and>valid_t t \<Longrightarrow> set (allBlocksGen (extendTreeGen t b)) =set ([b]@ allBlocksGen t)"
+lemma GenExtend : assumes "(extendTree t b \<noteq> t)\<and>valid_t t" shows "set (allBlocks (extendTree t b)) =set ([b]@ allBlocks t)"
+proof(cases "t")
+  case Leaf note LeafCase = this
+  then show ?thesis using assms LeafCase try
+    by simp
+next
+  case (Node x1 t1 t2)
+  then show ?thesis proof(induction "t1")
+    case Leaf
+    then show ?case using assms BaseExtend 
+      by simp
+  next
+    case (Node x1 t11 t12)
+    then show ?case using assms BaseExtend
+      by simp
+  qed
+qed
+(*
 proof(cases "t")
   case Leaf note Leaf = this
-  then show ?thesis sorry
+  then show ?thesis
+    using assms extendTree.simps(6) by blast
 next
   case (Node x1 t1 t2) note Nodex1 = this
   then show ?thesis proof(cases "t1")
     case Leaf note Leaft1 = this
     then show ?thesis proof(cases "t2")
       case Leaf
-      then show ?thesis using Nodex1 Leaft1 BaseExtend apply(auto) apply(simp add: GenBlock_def tree0_def Block1_def) sorry
+      then show ?thesis using assms Nodex1 Leaft1 BaseExtend
+        by auto
     next
       case (Node x21 x22 x23)
-      then show ?thesis using Nodex1 Leaft1 BaseExtend apply(auto) apply(simp add: GenBlock_def tree0_def Block1_def) sorry
+      then show ?thesis using Nodex1 Leaft1 BaseExtend assms apply(auto) done
     qed
   next
     case (Node x21 x22 x23) note Nodet1 = this
     then show ?thesis proof(cases"t2")
       case Leaf
-      then show ?thesis using Nodex1 Nodet1 BaseExtend apply(auto) apply(simp add: GenBlock_def tree0_def Block1_def) sorry
+      then show ?thesis using Nodex1 Nodet1 BaseExtend assms apply(auto) done
     next
       case (Node x21 x22 x23)
-      then show ?thesis using Nodex1 Nodet1 BaseExtend apply(auto) apply(simp add: GenBlock_def tree0_def Block1_def) sorry
+      then show ?thesis using Nodex1 Nodet1 BaseExtend assms apply(auto) done
     qed 
   qed
 qed
-
-(*proof(induction "t")
-  case Leaf
-  then show ?case 
-    apply(simp)
-    done
-next
-  case (Node x1 t1 t2) note Nodex1 = this
-  then show ?case proof (cases "t1")
-    case Leaf note Leaft1 = this
-    then show ?thesis proof (cases "t2")
-      case Leaf
-      then show ?thesis using Nodex1 Leaft1 BaseExtend
-        apply(simp add: Block1_def GenBlock_def tree0_def) 
-        apply(auto)
-          apply (metis GenBlock_def valid_chain.simps(2)) 
-         apply metis
-        by metis
-    next
-      case (Node x21 x22 x23)
-      then show ?thesis using Nodex1 Leaft1
-        apply(simp add: Nodex1 Leaft1 Block1_def GenBlock_def tree0_def) 
-        sorry
-    qed          
-  next
-    case (Node x21 x22 x23) note Nodet1 = this
-    then show ?thesis proof (cases "t2")
-      case Leaf
-      then show ?thesis using Nodex1 Nodet1
-        apply(simp add: Block1_def GenBlock_def tree0_def) 
-        sorry
-    next
-      case (Node x21 x22 x23)
-      then show ?thesis using Nodex1 Nodet1 
-        apply(simp add: Block1_def GenBlock_def tree0_def) 
-        apply(auto)
-        sorry 
-
-    qed
-  qed
-qed
 *)
-
